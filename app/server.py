a8559aee6da2b58c527f4fe68047243ea80e7bc2
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-12 08:44:08 +0000

    adding capibara/cavia classification stuff

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1xafkmrtUZaWWJks9Wp-htkphNEmHnez1'
+export_file_name = 'cavibara.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['cavia', 'capibara']
 path = Path(__file__).parent
 
 app = Starlette()
